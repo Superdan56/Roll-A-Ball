3efe57c2042a89493e891af3ae8c993d5cc81cab
Superdan56 <71519345+Superdan56@users.noreply.github.com>
2022-02-27 17:44:10 -0500

    I think I got the scripts fixed

--- Roll-A-Ball/Assets/Scripts/pickup_controller.cs
+++ Roll-A-Ball/Assets/Scripts/pickup_controller.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotator : MonoBehaviour {
+public class pickup_controller : MonoBehaviour {
 
 	// Before rendering each frame..
 	void Update () 
--- Roll-A-Ball/Assets/Scripts/player_controller.cs
+++ Roll-A-Ball/Assets/Scripts/player_controller.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+public class player_controller : MonoBehaviour
 {
     public float speed = 0;
 
