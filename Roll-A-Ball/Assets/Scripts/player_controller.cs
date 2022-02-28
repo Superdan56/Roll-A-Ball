3c2d862f199c8a5b0ec744dd3e9fd4b172a81d0b
Superdan56 <71519345+Superdan56@users.noreply.github.com>
2022-02-27 19:02:08 -0500

    finished tutorial (again)

--- Roll-A-Ball/Assets/Scripts/player_controller.cs
+++ Roll-A-Ball/Assets/Scripts/player_controller.cs
@@ -2,35 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
-public class player_controller : MonoBehaviour
-{
-    public float speed = 0;
+public class player_controller : MonoBehaviour {
 
-    private Rigidbody rb;
-
+    public float speed;
+    public TextMeshProUGUI CountText;
+    public GameObject winTextObject;
     private float movementX;
     private float movementY;
+    private Rigidbody rb;
+    private int count;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
     }
 
-    private void OnMove(InputValue movementValue)
-    {
-        Vector2 movementVector = movementValue.Get<Vector2>();
-
-        movementX = movementVector.x;
-        movementY = movementVector.y;
+    void FixedUpdate() {
+        Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
+        rb.AddForce (movement * speed);
     }
 
-    private void FixedUpdate()
-    {
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-
-        rb.AddForce(movement * speed);
+    void OnMove(InputValue value) {
+        Vector2 v = value.Get<Vector2>();  
+        movementX = v.x;
+    	movementY = v.y;
     }
 
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("PickUp")) {
+
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+
+        }
+    }
+
+    void SetCountText() {
+        CountText.text = "Count: " + count.ToString();
+        if (count >= 5) {
+            winTextObject.SetActive(true);
+        }
+    }
 }
