0145c6b0db1949d936b9eaaa44606d1be7065a05
Superdan56 <71519345+Superdan56@users.noreply.github.com>
2022-03-01 19:21:57 -0500

    jumping complete!
    also tidied up the code a lot, but the main thing is that the player can now jump, but can only do so while grounded.

--- Roll-A-Ball/Assets/Scripts/player_controller.cs
+++ Roll-A-Ball/Assets/Scripts/player_controller.cs
@@ -7,21 +7,16 @@ using TMPro;
 public class player_controller : MonoBehaviour {
 
     public float speed = 0;
-    public TextMeshProUGUI CountText;
     public GameObject winTextObject;
-    public float movementX;
-    public float movementY;
+    private float movementX;
+    private float movementY;
     public Rigidbody rb;
-    private int count;
-    public LayerMask groundLayers;
+    private bool isGrounded;
     public float jumpForce = 0;
-    public SphereCollider col;
 
     void Start() {
         rb = GetComponent<Rigidbody>();
-        col = GetComponent<SphereCollider>();
-        count = 0;
-        SetCountText();
+        isGrounded = false;
         winTextObject.SetActive(false);
     }
 
@@ -32,7 +27,7 @@ public class player_controller : MonoBehaviour {
     }
 
     void OnJump(InputValue value) {
-        if (IsGrounded()) {
+        if (isGrounded) {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
     }
@@ -42,24 +37,15 @@ public class player_controller : MonoBehaviour {
         rb.AddForce (movement * speed);
     }
 
-    void OnTriggerEnter(Collider other) {
-        if (other.gameObject.CompareTag("PickUp")) {
-
-            other.gameObject.SetActive(false);
-            count = count + 1;
-            SetCountText();
-
+    void OnCollisionEnter(Collision other) {
+        if (other.gameObject.CompareTag("Ground")) {
+            isGrounded = true;
         }
     }
 
-    void SetCountText() {
-        CountText.text = "Count: " + count.ToString();
-        if (count >= 5) {
-            winTextObject.SetActive(true);
+    void OnCollisionExit(Collision other) {
+        if (other.gameObject.CompareTag("Ground")) {
+            isGrounded = false;
         }
-    }
-
-    private bool IsGrounded() {
-        return Physics.CheckCapsule(col.bounds.center, new Vector3(col.bounds.center.x, col.bounds.min.y, col.bounds.center.z), col.radius * .9f, groundLayers);
     }
 }
